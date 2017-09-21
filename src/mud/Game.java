4c72c1361f14a26c3c57454d786a29e08eb779f7
rodav004 <32172844+rodav004@users.noreply.github.com>
2017-09-21 13:02:18 -0400

    Makes a single room containing one character, two items, no doors

--- src/mud/Game.java
+++ src/mud/Game.java
@@ -1,5 +1,28 @@
 package mud;
+import java.util.ArrayList;
 
 public class Game {
+	
+	public static void main(String args) {
+		
+		Item torch = new Item("torch","This is a burning stick.");
+		Item apple = new Item("apple","A yummy red fruit.");
+		
+		ArrayList<Item> items = new ArrayList<>();
+			items.add(apple);
+			items.add(torch);
+			
+		ArrayList<Item> inventory = new ArrayList<>();
+		
+		ArrayList<Character> people = new ArrayList<>();
+		
+		ArrayList<Door> doors = new ArrayList<>();
+		
+			
+		Room room1 = new Room("Nice room","This is a nice room.",items,people,doors);
+		Character person = new Character("Bob","Bob has brown hair.",room1,inventory);
+		
+		people.add(person);
+	}
 
 }
