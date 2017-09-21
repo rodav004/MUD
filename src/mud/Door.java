cbb1caba49ea63bbe86a824d26a8c11da1ee6a3c
michaelsavich <savichmichael@gmail.com>
2017-09-21 15:56:23 -0400

    Added getters to Door

--- src/mud/Door.java
+++ src/mud/Door.java
@@ -1,7 +1,15 @@
 package mud;
 
 public class Door {
+	
+	public String getDirection() {
+		return this.direction;
+	}
 	String direction;
+	
+	public Room getRoom() {
+		return this.room;
+	}
 	Room room;
 
 	public Door(Room room, String direction) {
