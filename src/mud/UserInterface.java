19e6865c177d7b7ae82b2633795accf46501ff87
rodav004 <32172844+rodav004@users.noreply.github.com>
2017-09-23 00:45:27 -0400

    I haven't totally figured out the scanner yet
    also this is really barebones, i wanted to be able to test the Character methods

--- src/mud/UserInterface.java
+++ src/mud/UserInterface.java
@@ -3,12 +3,19 @@ import java.util.Scanner;
 
 public class UserInterface {
 	
+	private static Scanner input;
+
 	public static void main(String[] args) {
-		String start = "Set your name.";
-		Scanner input = new Scanner(System.in);
+		
+		Game.start();
+		input = new Scanner(System.in);
+		String start = "Hello! Your name is Bob and you are in the Nice Room.";
 		System.out.println(start);
-		String name = input.nextLine();
-		System.out.println("Great! Your name is " + name + ".");
+		String direction = input.nextLine();
+		
+		String r = Game.person.move(direction);
+		System.out.println(r);
+	
 	}
 
 }
