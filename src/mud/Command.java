4aa7fa6c563bdaf49675f2ccab0706fd30043323
michaelsavich <savichmichael@gmail.com>
2017-09-26 15:45:26 -0400

    Cleanup Command.java

--- src/mud/Command.java
+++ src/mud/Command.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tres_caballeros.mud;
+package mud;
 
 /**
- *
+ * Command is basically a glorified tuple holding an Action and a String.
  * @author michaelsavich
  */
 public class Command {
