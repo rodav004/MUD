776bbdf214046f63f3ee2535c37cc837befc39c2
michaelsavich <savichmichael@gmail.com>
2017-09-26 12:28:25 -0400

    Add Constant.indexForDirection()

--- src/mud/Constant.java
+++ src/mud/Constant.java
@@ -6,6 +6,25 @@ package mud;
 	  public static int eastIndex = 1;
 	  public static int southIndex  = 2;
 	  public static int westIndex  = 3;
+		
+
+	  public static indexForDirection(Direction d) {
+	    switch (d) {
+              case NORTH: 
+	        return Constant.northIndex;
+	        break;
+	      case SOUTH:
+	        return Constant.southIndex;
+	        break;	
+	      case EAST:
+	        return Constant.eastIndex;
+	        break;
+	      case WEST:
+	        return Constant.westIndex;
+	        break;
+	      default:
+	        throw new AssertionErrorException();
+	  }
 
 	}
 
