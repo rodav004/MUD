891135545d1e31853eebff2185632f0786606603
michaelsavich <savichmichael@gmail.com>
2017-09-26 15:46:30 -0400

    Cleanup Action.java

--- src/mud/Action.java
+++ src/mud/Action.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tres_caballeros.mud;
+package mud;
 
 import java.util.Locale;
 import java.util.Optional;
