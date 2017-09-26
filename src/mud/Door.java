db9427573475408be1b66e79f0ed49c026b8f2ff
rodav004 <32172844+rodav004@users.noreply.github.com>
2017-09-26 12:25:46 -0400

    each door points to one room

--- src/mud/Door.java
+++ src/mud/Door.java
@@ -1,11 +1,10 @@
 package mud;
 
 public class Door {
-	Room room1;
-	Room room2;
+	Room room;
 
-	public Door(Room room1,Room room2) {
-		this.room1 = room1;
-		this.room2 = room2;
-	}	
+	public Door(Room room) {
+		this.room = room;
+	}
+	
 }
