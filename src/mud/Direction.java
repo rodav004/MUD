f53bdf6ce5d12a9c2a0b7a6e5a5d58d1bb5c5dcb
michaelsavich <savichmichael@gmail.com>
2017-09-26 17:03:22 -0400

    Refactor Direction.resolve()
    This is the style I've used in most of my other Java files, I don't know why this wrote it all weirdly the first time. I find this easier to debug and/or read, don't you?

--- src/mud/Direction.java
+++ src/mud/Direction.java
@@ -3,23 +3,31 @@ package mud;
 public enum Direction {
   NORTH, SOUTH, EAST, WEST;
   
-  public static Direction resolve(String input) {
-    switch (input.toLowerCase()) {
-      case "up":
-      case "north":
-        return Direction.NORTH;     
-      case "down":
-      case "south":
-        return Direction.SOUTH;
-	case "right":
-      case "east":
-        return Direction.EAST;       
-      case "left":
-      case "west":
-        return Direction.WEST;
-      default:
-        return null;
-      }
-   }
-   
+	public static Direction resolve(String input) {
+		Direction result;
+		switch (input.toLowerCase()) {
+		case "up":
+		case "north":
+			result = Direction.NORTH;
+			break;
+		case "down":
+		case "south":
+			result = Direction.SOUTH;
+			break;
+		case "right":
+		case "east":
+			result = Direction.EAST; 
+			break;
+		case "left":
+		case "west":
+			result = Direction.WEST;
+			break;
+		default:
+			result = null;
+			break;
+		}
+		
+		return result;
+	}
+	
 }
