68399fcee631f4d045a3eb4f0d2f1b9018cd2278
calam002 <32172869+calam002@users.noreply.github.com>
2017-09-26 12:30:30 -0400

    Update Character.java

--- src/mud/Character.java
+++ src/mud/Character.java
@@ -6,8 +6,6 @@ public class Character extends GameObject {
 	ArrayList<Item> inventory;
 	
 	public Character(String name, String description, Room location, ArrayList<Item> inventory) {
-		this.description = description;
-		this.name = name;
 		this.location = location;
 		this.inventory = inventory;
 	}
@@ -21,12 +19,21 @@ public class Character extends GameObject {
 		return itemNames;
 	}
 	
+	/*public String addItem(String item){
+		ArrayList<Item> roomItems = location.getItems();
+		for (Item roomItems: itemX) {
+			if (itemX.equal(item))
+				inventory.add(item);
+				roomItems.remove(item);
+		}
+	}*/
+	
 	//This isn't working right yet
 	public String move(String direction) {
 		String newRoom = null;
 		
 		if (direction.equals("north") || direction.equals("up")) {
-			Door exit = location.doors[0];
+			Door exit = location.doors[Constant.northIndex];
 			if (exit != null) {
 				newRoom = "You are in the " + exit.room1.name + ". "+ exit.room1.description;
 				this.location = exit.room1;
@@ -35,7 +42,7 @@ public class Character extends GameObject {
 			}
 		}
 		else if (direction.equals("south") || direction.equals("down")) {
-			Door exit = location.doors[2];
+			Door exit = location.doors[Constant.southIndex];
 			if (exit != null) {
 				newRoom = "You are in the " + exit.room2.name + ". "+ exit.room2.description;
 				this.location = exit.room2;
@@ -44,7 +51,7 @@ public class Character extends GameObject {
 			}
 		}
 		else if (direction.equals("east") || direction.equals("right")) {
-			Door exit = location.doors[1];
+			Door exit = location.doors[Constant.eastIndex];
 			if (exit != null) {
 				newRoom = "You are in the " + exit.room2.name + ". "+ exit.room2.description;
 				this.location = exit.room2;
@@ -53,7 +60,7 @@ public class Character extends GameObject {
 			}
 		}
 		else if (direction.equals("west") || direction.equals("left")) {
-			Door exit = location.doors[3];
+			Door exit = location.doors[Constant.westIndex];
 			if (exit != null) {
 				newRoom = "You are in the " + exit.room1.name + ". "+ exit.room1.description;
 				this.location = exit.room1;
