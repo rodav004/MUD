1fa331488c587eff6a3e3d9974ff56f42213b9ae
michaelsavich <savichmichael@gmail.com>
2017-09-26 12:35:42 -0400

    Rewrote Character.move()

--- src/mud/Character.java
+++ src/mud/Character.java
@@ -6,6 +6,8 @@ public class Character extends GameObject {
 	ArrayList<Item> inventory;
 	
 	public Character(String name, String description, Room location, ArrayList<Item> inventory) {
+		this.description = description;
+		this.name = name;
 		this.location = location;
 		this.inventory = inventory;
 	}
@@ -19,65 +21,24 @@ public class Character extends GameObject {
 		return itemNames;
 	}
 	
-	//add an item to a person's inventory
-	/*public String addItem(String item) {
-		Strint result = null;
-		ArrayList<Item> roomItems = location.getItems();
-		for (Item roomItems: itemX) {
-			if (itemX.equal(item))
-				inventory.add(item);
-				roomItems.remove(item);
-				result = "The item " + item " is now in your inventory. The description is " + item.getDescription();
-			else 
-				result = "The item is not in this room"
-		}
-		return result;
-	} */
-	
 	//This isn't working right yet
 	public String move(String direction) {
 		String newRoom = null;
 		
-		if (direction.equals("north") || direction.equals("up")) {
-			Door exit = location.doors[Constant.northIndex];
-			if (exit != null) {
-				newRoom = "You are in the " + exit.room1.name + ". "+ exit.room1.description;
-				this.location = exit.room1;
-			}
-			else {newRoom = "There is not a door in that direction!";
-			}
+		Direction theDirection = Direction.resolve(direction);
+		if (theDirection == null) {
+		     return "Sorry, that is not a valid direction!"; 
 		}
-		else if (direction.equals("south") || direction.equals("down")) {
-			Door exit = location.doors[Constant.southIndex];
-			if (exit != null) {
-				newRoom = "You are in the " + exit.room2.name + ". "+ exit.room2.description;
-				this.location = exit.room2;
-			}
-			else {newRoom = "There is not a door in that direction!";
-			}
+		int theIndex = Constant.indexForDirection(theDirection);
+		
+		Door exit = location.doors[theIndex];
+	        if (exit != null) {
+		     this.location = exit.room1;
+		     return "You are in the " + exit.room1.name + ". "+ exit.room1.description;
 		}
-		else if (direction.equals("east") || direction.equals("right")) {
-			Door exit = location.doors[Constant.eastIndex];
-			if (exit != null) {
-				newRoom = "You are in the " + exit.room2.name + ". "+ exit.room2.description;
-				this.location = exit.room2;
-			}
-			else {newRoom = "There is not a door in that direction!";
-			}
+		else {
+		     return "There is not a door in that direction!";
 		}
-		else if (direction.equals("west") || direction.equals("left")) {
-			Door exit = location.doors[Constant.westIndex];
-			if (exit != null) {
-				newRoom = "You are in the " + exit.room1.name + ". "+ exit.room1.description;
-				this.location = exit.room1;
-			}
-			else {newRoom = "There is not a door in that direction!";
-			}
-		}
-		else{
-			newRoom = "Sorry, that is not a valid direction!";
-		}
-		return newRoom;
 	}
 	
 	
