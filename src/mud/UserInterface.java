93e91a6224179ec5fcf1f165003f9c46e5e45c70
michaelsavich <savichmichael@gmail.com>
2017-09-27 01:26:35 -0400

    Silence compilation errors...

--- src/mud/UserInterface.java
+++ src/mud/UserInterface.java
@@ -6,16 +6,16 @@ public class UserInterface {
 	private static Scanner input;
 
 	public static void main(String[] args) {
-		
-		Game.start();
+		Game game = new Game()
+		game.start();
 
-		String start = "Hello! Your name is Bob and you are in the Nice Room.";
-		System.out.println(start);
-		input = new Scanner(System.in);
-		String direction = input.next();
-		direction = direction.toString();
-		String r = Game.person.move(direction);
-		System.out.println(r);
+// 		String start = "Hello! Your name is Bob and you are in the Nice Room.";
+// 		System.out.println(start);
+// 		input = new Scanner(System.in);
+// 		String direction = input.next();
+// 		direction = direction.toString();
+// 		String r = Game.person.move(direction);
+// 		System.out.println(r);
 	}
 
 }
