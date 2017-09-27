9854218fb04e65770d93f883c0334b7ad385629e
michaelsavich <savichmichael@gmail.com>
2017-09-26 20:15:18 -0400

    Add function to make List<Item> from Items[]

--- src/mud/Items.java
+++ src/mud/Items.java
@@ -1,5 +1,8 @@
 package mud;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public enum Items {
     TORCH, APPLE, ROCK, PEN;
     
@@ -11,6 +14,14 @@ public enum Items {
         case PEN:   return new Item("pen","Useful for writing.");
         }
         throw new AssertionError("Could not create an Item for " + this.name() + "!"); 
+    }
+    
+    public static List<Item> make(Items[] theItems) {
+        List<Item> result = new ArrayList<>();
+        for (Items theItem : theItems) {
+            result.add(theItem.make());
+        }
+        return result;
     }
     
 }
