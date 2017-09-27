4275adad0e5cb20b822eeff83f4c87bcc0308d5f
michaelsavich <savichmichael@gmail.com>
2017-09-27 10:56:33 -0400

    Add starter implementation for Player and Mob

--- src/mud/Mob.java
+++ src/mud/Mob.java
@@ -1,5 +1,14 @@
 package mud;
-
-public class Mob {
-
+import java.util.List;
+import java.util.ArrayList;
+public class Mob extends Character {
+	public Mob(String name, String description, Room location, List<Item> inventory) {
+		super(name, description,location,inventory);
+	}
+	public Mob(String name, String description, Room location) {
+		super(name, description, location, new ArrayList<Item>());
+	}
+	public Mob(String name, String description) {
+		super(name,description,null);
+	}	
 }
--- src/mud/Player.java
+++ src/mud/Player.java
@@ -1,5 +1,15 @@
 package mud;
-
-public class Player {
+import java.util.List;
+import java.util.ArrayList;
+public class Player extends Character {
+	public Player(String name, String description, Room location, List<Item> inventory) {
+                super(name, description,location,inventory);
+        }
+        public Player(String name, String description, Room location) {
+                super(name, description, location, new ArrayList<Item>());
+        }
+        public Player(String name, String description) {
+                super(name,description,null);
+        }
 
 }
