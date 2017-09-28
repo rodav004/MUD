53219b969a4d9e898e7484240bc606ab751f20a9
michaelsavich <savichmichael@gmail.com>
2017-09-28 12:25:14 -0400

    Add synonyms to Action

--- src/mud/Action.java
+++ src/mud/Action.java
@@ -15,12 +15,32 @@ public enum Action {
 	public static Action resolve(String str) {
 		Action result;
 		switch (str.toUpperCase(Locale.ROOT)) {
+		case "GO":
+		case "TRAVEL":
+		case "JOURNEY":
+		case "VENTURE":
+		case "WALK":
+		case "RUN":
+		case "DASH":
+		case "SPINDASH":
+		case "LOCOMOTE":
 	     	case "MOVE":
 			result = Action.MOVE;
 			break;
+		case "GET":
+		case "PICK":
+		case "ACQUIRE":
+		case "PILFER":
+		case "ASSIMILATE":
 	     	case "TAKE":
 			result = Action.TAKE;
 		  	break;
+		case "HIT":
+		case "ASSAULT":
+		case "DAMAGE":
+		case "PUNCH":
+		case "KICK":
+		case "DESTROY":
 	        case "ATTACK":
 			result = Action.ATTACK;
 		  	break;
