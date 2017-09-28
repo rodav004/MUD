dd65a0863ce588476d55dce9738916bcef1054e6
michaelsavich <savichmichael@gmail.com>
2017-09-28 12:18:29 -0400

    Improved spacing in Action.java

--- src/mud/Action.java
+++ src/mud/Action.java
@@ -8,28 +8,28 @@ import java.util.Optional;
  * @author michaelsavich
  */
 public enum Action {
-    MOVE,
-    TAKE,
-    ATTACK;
+	MOVE,
+	TAKE,
+	ATTACK;
     
-    public static Action resolve(String str) {
-	 Action result;
-	 switch (str.toUpperCase(Locale.ROOT)) {
-	     case "MOVE":
-		  result = Action.MOVE;
-		  break;
-	     case "TAKE":
-		  result = Action.TAKE;
-		  break;
-	     case "ATTACK":
-		  result = Action.ATTACK;
-		  break;
-	     default:
-		  result = null;
-		  break;
-	 }
-	return result;	 
-    }
+	public static Action resolve(String str) {
+		Action result;
+		switch (str.toUpperCase(Locale.ROOT)) {
+	     	case "MOVE":
+			result = Action.MOVE;
+			break;
+	     	case "TAKE":
+			result = Action.TAKE;
+		  	break;
+	        case "ATTACK":
+			result = Action.ATTACK;
+		  	break;
+	   	default:
+			result = null;
+			break;
+	 	}
+		return result;	 
+	}
 }
 
 
