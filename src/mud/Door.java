439540328904208dba8953ab7bf1d667b81be640
michaelsavich <savichmichael@gmail.com>
2017-09-28 17:57:28 -0400

    Add javadoc to Door

--- src/mud/Door.java
+++ src/mud/Door.java
@@ -1,8 +1,21 @@
 package mud;
-
+/**
+ * A Door points to a Room.
+ * Although it does not inherit from GameObject,
+ * Door is considered to be a model class.
+ */
 public class Door {
+	/**
+	 * The Room this Door points to.
+         * This is a public final field, so it cannot be modified.
+	 */
 	public final Room room;
 	
+	/**
+	 * Creates a new Door that points to a room.
+	 * @param room The room this Door points to.
+	 * @return The newly created Door.
+	 */	
 	public Door(Room room) {
 		this.room = room;
 	}
